929cf5311ae2065d6fbbe12e8ac675718d927e33
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2022-10-24 12:53:38 +0100

    ch(setup): basic server/project setup

---
+++ server.js
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
