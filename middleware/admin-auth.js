68b94561281a601aadba822d22607634c3e0bcf1
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2022-10-30 01:32:01 +0100

    rf: refactor codes to follow best practice

--- middleware/admin-auth.js
+++ middleware/admin-auth.js
@@ -1,11 +1,9 @@
 const adminAuth = (req, res, next) => {
-  if (req.path === '/create-user') {
-    if (req.body.isAdmin === false) {
-      return res.status(403).json({
-        status: 'error',
-        error: 'Only an admin can perform this action',
-      });
-    }
+  if (req.body.isAdmin === false) {
+    return res.status(403).json({
+      status: 'error',
+      error: 'Only an admin can register employees',
+    });
   }
 
   next();
