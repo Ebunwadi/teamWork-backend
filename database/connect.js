10b3b791ddd46ab9014f557f5362f6100b94d6c3
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2022-11-17 23:51:16 +0100

    connect to render postgresql server

--- database/connect.js
+++ database/connect.js
@@ -4,12 +4,19 @@ import * as pg from 'pg';
 const { Pool } = pg.default;
 dotenv.config();
 
-const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: process.env.CONNECT_PASSWORD,
-  port: 5432,
-  database: 'teamwork',
-});
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: 'postgres',
+//   password: process.env.CONNECT_PASSWORD,
+//   port: 5432,
+//   database: 'teamwork',
+// });
 
+const pool = new Pool({
+  host: process.env.RENDER_HOST,
+  user: 'ebube',
+  password: process.env.RENDER_PASSWORD,
+  port: 5432,
+  database: 'teamwork_fcgt',
+});
 export default pool;
