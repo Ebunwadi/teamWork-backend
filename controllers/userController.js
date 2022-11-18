c21660851e9c1935babf8f19d635342b77c67fd0
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2022-11-18 01:31:06 +0100

    test render deployment

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,51 +3,45 @@ import bcrypt from 'bcrypt';
 import pool from '../database/connect.js';
 
 export const createUser = async (req, res) => {
-  try {
-    const {
-      firstName, lastName, email, password, gender, jobRole, department, address, isAdmin,
-    } = req.body;
-  
-    const checkUser = await pool.query('SELECT * FROM users WHERE email=$1', [email]);
-    if (checkUser.rows.length !== 0) {
-      return res.status(401).json({
-        status: 'error',
-        error: 'User already registered',
-      });
-    }
-  
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword = await bcrypt.hash(password, salt);
-  
-    const newUser = await pool.query(
-      `INSERT INTO users (first_name, last_name, email, password, gender, job_role, department, address, is_admin) 
-          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-      [firstName, lastName, email, hashedPassword, gender, jobRole, department, address, isAdmin],
-    );
-  
-    const userId = newUser.rows[0].id;
-  
-    const payload = {
-      userId,
-      email,
-      isAdmin,
-    };
-  
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '72h' });
-    return res.status(201).json({
-      status: 'success',
-      data: {
-        message: 'Account created successfully',
-        token,
-        payload,
-      },
+  const {
+    firstName, lastName, email, password, gender, jobRole, department, address, isAdmin,
+  } = req.body;
+
+  const checkUser = await pool.query('SELECT * FROM users WHERE email=$1', [email]);
+  if (checkUser.rows.length !== 0) {
+    return res.status(401).json({
+      status: 'error',
+      error: 'User already registered',
     });
-  } catch (error) {
-    console.log(error);
-    res.send('error')
   }
-  
+
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = await pool.query(
+    `INSERT INTO users (first_name, last_name, email, password, gender, job_role, department, address, is_admin) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
+    [firstName, lastName, email, hashedPassword, gender, jobRole, department, address, isAdmin],
+  );
+
+  const userId = newUser.rows[0].id;
+
+  const payload = {
+    userId,
+    email,
+    isAdmin,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '72h' });
+  return res.status(201).json({
+    status: 'success',
+    data: {
+      message: 'Account created successfully',
+      token,
+      payload,
+    },
+  });
 };
 
 export const loginUser = async (req, res) => {
