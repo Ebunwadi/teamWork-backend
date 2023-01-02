bcf193c860714f6879de27f7c8ad4e600fb1ec9a
Ebunwadi <111129501+Ebunwadi@users.noreply.github.com>
2023-01-02 23:44:29 +0100

    ft(functional login page): implement login and forgot password functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -164,8 +164,8 @@ export const resetPassword = async (req, res) => {
 
 export const passwordReset = async (req, res) => {
   const { id, token } = req.params;
-  jwt.verify(token, process.env.JWT_SECRET);
   try {
+    jwt.verify(token, process.env.JWT_SECRET);
     const { password } = req.body;
     const user = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
     if (user.rows.length === 0) {
@@ -189,7 +189,7 @@ export const passwordReset = async (req, res) => {
     console.log(error);
     return res.status(401).json({
       status: 'error',
-      error: 'something is wrong ',
+      error,
     });
   }
 };
